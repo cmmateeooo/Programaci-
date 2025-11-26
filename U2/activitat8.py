5c07f606034960bf34cbe8ac4eab96b12859baee
kali <kali@kali.kali>
2025-11-26 13:08:27 +0100

    U2

---
+++ U2/activitat8.py
@@ -0,0 +1,6 @@
+try:
+num1 = int(input("Introdueix el primer numero:"))
+num1 = int(input("Introdueix el primer numero:"))
+res = num1/num2
+print(f"El resultat de la divisió és: {resultat}")
+except 
--- U2/temperatura.py
+++ U2/temperatura.py
@@ -1,2 +1,15 @@
 temperatura= float(input("Passam la temperatura"))
-nuvolat= input("Esta nuvolat?")
+nuvolat= input("Esta nuvolat? (si/no): ").strip().lower()
+
+if temperatura < 0:
+    print("Fa un fred polar!")
+elif 0 <= temperatura <= 15:
+    if nuvolat == "si":
+        print("Fa fred i el dia esta trist.")
+    elif nuvolat == "no":
+        print("Fa fresqueta però el sol alegra el dia")
+    else:
+        print("Resposta no valida per a nuvolat.")
+elif 16 <= temperatura <= 25
+if nuvolat=="si":
+    print("Temperatura agradable, però potser ploga.")
