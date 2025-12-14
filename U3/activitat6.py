08fb06bbb86b749d915edc94db3ae9f15fcf1703
kali <kali@kali.kali>
2025-12-14 19:58:39 +0100

    U3

--- U3/activitat6.py
+++ U3/activitat6.py
@@ -5,10 +5,11 @@ try:
     with open('dades_exercici.json', 'r') as fitxer:
         dades = json.load(fitxer)
 
-    dades["assignatures"].append("Programacio")
+    dades[0]["assignatures"].append("Programacio")
 
     with open('dades_exercici.json', 'w') as fitxer:
         json.dump(dades, fitxer, indent=4)
+        print("asignatura añadida.")
 
 except FileNotFoundError:
     print("No s'ha trobat l'arxiu")
