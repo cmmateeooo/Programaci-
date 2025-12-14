78325abd55e46783abc4e3e60c5bdff8e2f8a49f
kali <kali@kali.kali>
2025-12-14 18:32:46 +0100

    U3

--- U3/activitat6.py
+++ U3/activitat6.py
@@ -1,12 +1,19 @@
 import json
 
 try:
+    # Leer el archivo JSON
     with open('dades_exercici.json', 'r') as fitxer:
-       dades = json.load(fitxer)
+        dades = json.load(fitxer)
 
-    dades_noves = {"Asignatura:" "Programacio"}
+    # Añadir una nueva asignatura
+    dades["assignatures"].append("Programacio")
+
+    # Guardar los cambios en el mismo archivo
     with open('dades_exercici.json', 'w') as fitxer:
-        json.dump(dades_noves, fitxer, indent=4)
+        json.dump(dades, fitxer, indent=4)
 
 except FileNotFoundError:
     print("No s'ha trobat l'arxiu")
+
+except json.JSONDecodeError:
+    print("El fitxer no conté un JSON vàlid")
