1e6ae16f1f257c70dec4c2e3a9e5a1b69bbb63b7
kali <kali@kali.kali>
2026-03-04 13:39:51 +0100

    Activitats

--- U4/Exercici3.py
+++ U4/Exercici3.py
@@ -1,9 +1,13 @@
 notes = [5,7,9,4,6,3,1,8,2,10]
 
+#Variables de les notes abans del bucle.
+
 aprovats = 0
 suspesos = 0
 
 print("Llista de notes:")
+
+#Bucle for per fer recompter de aprovats i suspesos.
 
 for n in notes:
     print(n)
